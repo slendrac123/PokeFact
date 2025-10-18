6b34e275841935535daa99beecbbd02aa7b8c976
Jacel Thomás Enciso Pinzon <jencisop@unal.edu.co>
2025-10-17 22:15:17 -0500

    fix db settings and credentials

--- pokefact-back/app/database.py
+++ pokefact-back/app/database.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# Datos de conexión
-DATABASE_URL = "postgresql://admin123:admin123@localhost:5432/pokefact_db"
+# Cargar las variables del archivo .env
+load_dotenv()
+
+# Leerlas.
+USER = os.getenv("POSTGRES_USER")
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB = os.getenv("POSTGRES_DB")
+HOST = os.getenv("POSTGRES_HOST", "localhost")
+PORT = os.getenv("POSTGRES_PORT", "5432")
+
+# Crear la URL dinámica
+DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 
 # Crear motor y sesión
 engine = create_engine(DATABASE_URL)
